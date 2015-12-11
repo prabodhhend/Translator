8651e071ed57a46dc7db53dec61ca8ef088abd06
Prabodh Hend <prabodhhend@rediffmail.com>
2015-12-11 14:53:11 +0530

    Update Language.java

--- Language.java
+++ Language.java
@@ -1,4 +1,13 @@
-public enum Languages{
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-
+class Translator
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan = new Scanner(System.in);
+		String inputJsonString = scan.nextLine();
+		System.out.println(inputJsonString);
+	}
 }
